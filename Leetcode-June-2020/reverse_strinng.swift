1ddb39b1d5456a75af10868e221da77cd9375448
Nikhil0487 <nikhilduggirala95@gmail.com>
2020-06-17 08:32:22 +0530

    Two city schedule solution

---
+++ Leetcode-June-2020/reverse_strinng.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Leetcode-June-2020
+//
+//  Created by sri-7348 on 6/15/20.
+//  Copyright © 2020 Nikhi. All rights reserved.
+//
+
+import Foundation
---
+++ Leetcode-June-2020/two_city_scheduling.swift
@@ -0,0 +1,27 @@
+//
+//  two_city_scheduling.swift
+//  Leetcode-June-2020
+//
+//  Created by sri-7348 on 6/4/20.
+//  Copyright © 2020 Nikhi. All rights reserved.
+//
+
+import Foundation
+
+func twoCitySchedCost(_ costs: [[Int]]) -> Int {
+    let extraCost = costs.sorted { 
+        print($0[0] - $0[1], $1[0] - $1[1])
+        return ($0[0] - $0[1] < $1[0] - $1[1])
+    }
+    let minCostsOfA = extraCost.prefix(costs.count/2).compactMap {
+        $0[0]
+    }.reduce(0) { x, y in
+        x+y
+    }
+    let minCostOfB = extraCost.suffix(costs.count/2).compactMap {
+        $0[1]
+    }.reduce(0) { x, y in
+        x+y
+    }
+    return minCostsOfA+minCostOfB
+}
